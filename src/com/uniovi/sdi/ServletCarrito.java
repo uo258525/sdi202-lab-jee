a663dbc3f4f1e68d22cbc5d20b596b31110b5563
Elena <uo258525@uniovi.es>
2019-02-03 23:39:31 +0100

    SDI – 4 MVC, Modelo Vista Controlador

--- src/com/uniovi/sdi/ServletCarrito.java
+++ src/com/uniovi/sdi/ServletCarrito.java
@@ -32,17 +32,22 @@ public class ServletCarrito extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		HttpSession session=request.getSession();
-		HashMap <String,Integer> carrito=(HashMap<String, Integer>)request.getSession().getAttribute("carrito");
+		HashMap<String,Integer> carrito =
+		 (HashMap<String,Integer>) request.getSession().getAttribute("carrito");
 		// No hay carrito, creamos uno y lo insertamos en sesión
-		if(carrito==null) {
-			carrito=new HashMap<String,Integer>();
-			request.getSession().setAttribute("carrito", carrito);
+		if (carrito == null) {
+		carrito = new HashMap<String,Integer>();
+		 request.getSession().setAttribute("carrito", carrito);
 		}
-		
 		String producto = request.getParameter("producto");
-		if(producto!=null) {
-			insertarEnCarrito(carrito,producto);
+		if ( producto != null){
+		insertarEnCarrito(carrito, producto);
 		}
+		// Retornar la vista con parámetro "carrito"
+		request.setAttribute("paresCarrito", carrito);
+		getServletContext().getRequestDispatcher("/vista-carrito.jsp").forward(request,
+		response);
+
 		
 
 	response.setCharacterEncoding("UTF-8");
