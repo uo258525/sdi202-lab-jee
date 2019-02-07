13d6131fef943715bccb5f93df784f49f02ca0c2
Elena <uo258525@uniovi.es>
2019-02-07 13:31:07 +0100

    problems with html solved.

--- src/com/uniovi/sdi/ServletCarrito.java
+++ src/com/uniovi/sdi/ServletCarrito.java
@@ -17,70 +17,69 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/incluirEnCarrito")
 public class ServletCarrito extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletCarrito() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public ServletCarrito() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		HttpSession session=request.getSession();
-		HashMap<String,Integer> carrito =
-		 (HashMap<String,Integer>) request.getSession().getAttribute("carrito");
+		HttpSession session = request.getSession();
+		HashMap<String, Integer> carrito = (HashMap<String, Integer>) request.getSession().getAttribute("carrito");
 		// No hay carrito, creamos uno y lo insertamos en sesión
 		if (carrito == null) {
-		carrito = new HashMap<String,Integer>();
-		 request.getSession().setAttribute("carrito", carrito);
+			carrito = new HashMap<String, Integer>();
+			request.getSession().setAttribute("carrito", carrito);
 		}
 		String producto = request.getParameter("producto");
-		if ( producto != null){
-		insertarEnCarrito(carrito, producto);
+		if (producto != null) {
+			insertarEnCarrito(carrito, producto);
 		}
 		// Retornar la vista con parámetro "carrito"
 		request.setAttribute("paresCarrito", carrito);
-		getServletContext().getRequestDispatcher("/vista-carrito.jsp").forward(request,
-		response);
+		getServletContext().getRequestDispatcher("/vista-carrito.jsp").forward(request, response);
 
-		
-
-	response.setCharacterEncoding("UTF-8");
-	response.setContentType("text/html");
-	PrintWriter out = response.getWriter();
-	out.println("<HTML>");
-	out.println("<HEAD><TITLE>Tienda SDI: carrito</TITLE></HEAD>");
-	out.println("<BODY>");
-	out.println(carritoEnHTML(carrito)+"<br>");
-	out.println("<a href=\"\"index.jsp\">Volver</a></BODY></HTML>");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<HTML>");
+		out.println("<HEAD><TITLE>Tienda SDI: carrito</TITLE></HEAD>");
+		out.println("<BODY>");
+		out.println(carritoEnHTML(carrito) + "<br>");
+		out.println("<a href=\"\"index.jsp\">Volver</a></BODY></HTML>");
 	}
-	
-	
-	private void insertarEnCarrito(Map<String,Integer> carrito, String claveProducto) {
-		if (carrito.get(claveProducto)==null)
-		carrito.put(claveProducto, new Integer(1));
+
+	private void insertarEnCarrito(Map<String, Integer> carrito, String claveProducto) {
+		if (carrito.get(claveProducto) == null)
+			carrito.put(claveProducto, new Integer(1));
 		else {
-		int numeroArticulos=(Integer)carrito.get(claveProducto).intValue();
-		carrito.put(claveProducto,new Integer(numeroArticulos+1));
+			int numeroArticulos = (Integer) carrito.get(claveProducto).intValue();
+			carrito.put(claveProducto, new Integer(numeroArticulos + 1));
 		}
-		}
-	private String carritoEnHTML(Map<String,Integer> carrito) {
-		String carritoEnHTML="";
-		for (String key:carrito.keySet())
-		carritoEnHTML+="<p>["+key+"], "+carrito.get(key)+" unidades</p>";
+	}
+
+	private String carritoEnHTML(Map<String, Integer> carrito) {
+		String carritoEnHTML = "";
+		for (String key : carrito.keySet())
+			carritoEnHTML += "<p>[" + key + "], " + carrito.get(key) + " unidades</p>";
 		return carritoEnHTML;
-		}		
-	
+	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
--- src/com/uniovi/sdi/ServletSaludo.java
+++ src/com/uniovi/sdi/ServletSaludo.java
@@ -15,43 +15,45 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ServletSaludo")
 public class ServletSaludo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	int contador=0;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-	
-	
-    public ServletSaludo() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	int contador = 0;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	public ServletSaludo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		out.println("<HTML>");
 		out.println("<HEAD><TITLE> Hola Mundo!</TITLE></HEAD>");
 		out.println("<BODY>");
 		String nombre = (String) request.getParameter("nombre");
-		if(nombre!=null) {
-			out.println ("Hola "+ nombre + "<br>");
-			}
-		
-	
+		if (nombre != null) {
+			out.println("Hola " + nombre + "<br>");
+		}
+
 		out.println("</BODY></HTML>");
-		
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
